f3dbbc2a30755dd60fce45d7ce52fd3fb4eedc7f
Matias Fuentes <matiasj.fuentes@gmail.com>
2017-07-15 00:20:15 +1000

    Changes

--- app/src/main/java/com/nedelu/juntada/activity/NewGroupActivity.java
+++ app/src/main/java/com/nedelu/juntada/activity/NewGroupActivity.java
@@ -1,12 +1,17 @@
 package com.nedelu.juntada.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -40,10 +45,20 @@ public class NewGroupActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int permissionCheck = ContextCompat.checkSelfPermission(NewGroupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+                    if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(NewGroupActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    } else {
+                        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(i, RESULT_LOAD_IMAGE);
+                    }
+                }
+
             }
         });
+
+
 
         Button createButton = (Button) findViewById(R.id.create_button);
 
@@ -59,8 +74,6 @@ public class NewGroupActivity extends AppCompatActivity {
                     group.setName(editTextName.getText().toString());
                     group.setImageUrl("http://thomrainer.com/wp-content/uploads/2013/10/Start-New-Groups.jpg");
 
-                    //TODO SEND GROUP TO SERVER
-
                     GroupService.getInstance(NewGroupActivity.this).createGroup(userId, group, NewGroupActivity.this);
                 } else {
                     Snackbar.make(view, "All fields must be completed!.", Snackbar.LENGTH_SHORT)
@@ -69,6 +82,18 @@ public class NewGroupActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+                // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                } else {
+
+                }
+        }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
